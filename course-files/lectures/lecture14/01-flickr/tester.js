9ac8a91bbb34891cf8d0cbc9093d174ef15bf69d
Sarah <vanwars@gmail.com>
2022-05-11 15:48:51 -0500

    updates

--- course-files/lectures/lecture14/01-flickr/tester.js
+++ course-files/lectures/lecture14/01-flickr/tester.js
@@ -6,16 +6,18 @@ const getPhotos = () => {
     // everytime it is invoked. Change the value of
     // the tag to get different data:
     fetch(serverURL + '?tags=nature')
-        .then(response => response.json())
+        .then(response => {
+            response.json()
+        })
         .then(doSomethingWithTheData);
 };
 
-const doSomethingWithTheData = (response) => {
+const doSomethingWithTheData = (data) => {
     // this function executes once the data has been 
     // pulled down from the server:
-
+    results = data;
     console.log('The Flickr data is stored in a variable called "results":')
-    console.log(results);
+    console.log(data);
 };
 
 // invoke the function when the page initializes:
